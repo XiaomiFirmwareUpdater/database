11c1c4377dfe048631d167fad83c87c15c5da17a
yshalsager <ysh-alsager@hotmail.com>
2020-11-01 18:56:59 +0200

    firmware: Add a method to get all firmware updates
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- firmware.py
+++ firmware.py
@@ -3,6 +3,9 @@ Database Firmware Updates related functions
 """
 from typing import List
 
+from sqlalchemy.engine import result
+from sqlalchemy.sql.functions import concat
+
 from . import session
 from .models.device import Device
 from .models.firmware_update import Update
@@ -25,3 +28,18 @@ def update_in_db(codename, version) -> bool:
     :return: True if the update is already in the database
     """
     return bool(session.query(Update).filter_by(codename=codename).filter_by(version=version).count() >= 1)
+
+
+def get_all_updates() -> result:
+    """
+    SELECT CONCAT(d.name, ' ', d.region) as name, firmware.codename, version,
+           android, branch, filename, size, md5, date
+    from firmware
+             JOIN devices d on firmware.codename = d.codename
+    GROUP BY md5
+    :return: list of firmware results
+    """
+    return session.query(
+        concat(Device.name, ' ', Device.region).label('name'), Update.codename, Update.version,
+        Update.android, Update.branch, Update.filename, Update.size, Update.md5, Update.date
+    ).join(Device, Update.codename == Device.codename).group_by(Update.md5).all()
